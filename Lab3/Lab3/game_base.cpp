81311621d03e2eb6d29cb80e1c520185b311e683
Yifu Ma <yifuma@wustl.edu>
2014-11-05 01:47:43 -0600

    copy prompt method for base class

--- Lab3/Lab3/game_base.cpp
+++ Lab3/Lab3/game_base.cpp
@@ -74,3 +74,27 @@ int gameBase::play() {
 	/*Should be impossible to reach here*/
 	return programLogicError;
 }
+
+void gameBase::prompt(int& a, int& b) {
+	string input;
+	/*Inital prompt for input*/
+	cout << "Enter coordinates (\"x,y\") or quit game (\"quit\"):" << endl;
+	std::cin >> input;
+	/*Throw up if user has chosen to quit*/
+	if (input == "quit") {
+		throw (int)userExit;
+		return;
+	}
+	/*Format the input string*/
+	replace(input.begin(), input.end(), ',', ' ');
+	/*If the string is not valid, reprompt the user until it is*/
+	while (!((istringstream)input >> a >> b)) {
+		cout << "Not a valid input, Enter coordinates (\"x,y\") or quit game (\"quit\"):" << endl;
+		std::cin >> input;
+		if (input == "quit") {
+			throw (int)userExit;
+			return;
+		}
+		replace(input.begin(), input.end(), ',', ' ');
+	}
+}
--- Lab3/Lab3/game_base.h
+++ Lab3/Lab3/game_base.h
@@ -19,7 +19,7 @@ public:
 	virtual void print() = 0;
 	virtual bool done() = 0;
 	virtual bool stalemate() = 0;
-	virtual void prompt() = 0;
+	virtual void prompt(int&, int&);
 	virtual void turn() = 0;
 	
 	gameBase(std::vector<game_piece>, int, int);
